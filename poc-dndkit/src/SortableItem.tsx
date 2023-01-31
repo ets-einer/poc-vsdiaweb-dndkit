6a119b7aedca614e783eab8800317bdf1e423e9c
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-31 09:40:51 -0300

    📦 NEW: Refactoring sortable item
    Co-authored-by:Vinicius Prates<viniprates2211@gmail.com>
    Co-authored-by: breno Florencio <brenodelbem@gmail.com>
    Co-authored-by: Vinicius Ferreira <vinijoseferreira38@gmail.com>

--- poc-dndkit/src/SortableItem.tsx
+++ poc-dndkit/src/SortableItem.tsx
@@ -1,22 +1,26 @@
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities";
+import { Raia } from "./Raia";
 
-export function SortableItem(props:any){
+export function SortableItem(props: any) {
     const
-    {
-        attributes,
-        listeners,
-        setNodeRef,
-        transform,
-        transition
-    } = useSortable({id:props.id});
+        {
+            attributes,
+            listeners,
+            setNodeRef,
+            transform,
+            transition,
+        } = useSortable({ id: props.id });
 
-    const style ={
-        padding:'5px',
-        background:'#f2f2f2',
-        margin:'10px',
+    const style = {
+        background: '#f2f2f2',
+        margin: '10px',
         transform: CSS.Transform.toString(transform),
-        transition
+        transition,
+        cursor: 'pointer',
+        width: "200px",
+        padding: '1rem',
+        borderRadius: '5px'
     }
 
     return (
