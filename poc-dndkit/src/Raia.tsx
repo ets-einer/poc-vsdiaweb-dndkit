ac8b87a99420007ba63374097a2fe77df38321f5
breno936 <brenodelbem@gmail.com>
2023-02-02 11:21:19 -0300

    Modificação de metodo mover listas

--- poc-dndkit/src/Raia.tsx
+++ poc-dndkit/src/Raia.tsx
@@ -3,8 +3,10 @@ import { CSS } from "@dnd-kit/utilities";
 import { SortableItem } from "./SortableItem";
 import { useState } from "react";
 import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core";
+import { Lista } from "./Lista";
 
 export function Raia(props:any){
+    const list = Lista();
     const [pessoas, setPessoas] = useState(["Miura", "Camuda", "Igor", "Prates"]);
     const
     {
@@ -26,23 +28,41 @@ const slaporra={
 
 function handleDragEnd(event: any) {
     console.log(event.collisions)
+ 
     const { active, over } = event
 
     if (active.id !== over.id) {
-      setPessoas((items) => {
-        const activeItems = items.indexOf(active.id)
-        const overItems = items.indexOf(over.id)
+        
+      const raiaSelecionada = list.listaRaias[props.id -1]
+      const activeItems = raiaSelecionada.elementos.indexOf(active.id)
+      const activeName = active.id;
+      const overName = over.id;
+      const overItems = raiaSelecionada.elementos.indexOf(over.id)
+      console.log(raiaSelecionada.elementos);
 
-        return arrayMove(items, activeItems, overItems)
+      raiaSelecionada.elementos[overItems] = activeName;
+      raiaSelecionada.elementos[activeItems] = overName;
+      console.log(raiaSelecionada.elementos);
+      raiaSelecionada.elementos.map(() =>{
+        return
       })
+
+    //   list.setRaias((items) => {                                                                
+       
+    //     let arrItens = items[props.id-1]  
+    //     console.log(arrItens.elementos.indexOf(active.id))
+    //     console.log(arrItens.elementos.indexOf(over.id))
+       
+    //     // return arrayMove(items, activeItems, overItems)
+    //   })
     }
   }
 
 return (
     <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-        <SortableContext items={pessoas} strategy={horizontalListSortingStrategy}>
+        <SortableContext items={list.listaRaias[props.id - 1].elementos} strategy={horizontalListSortingStrategy}>
     <div style={slaporra} ref={setNodeRef} {...attributes} {...listeners}>
-        {pessoas.map(pessoa => <SortableItem key={pessoa} id={pessoa} />)}
+        {list.listaRaias[props.id - 1].elementos.map(pessoa => <SortableItem key={pessoa} id={pessoa} />)}
     </div>
     </SortableContext>
     </DndContext>
